6a18fd2e7b94916c781d5b5ef1370a962e588a0e
Paul Ockenfuss <42680748+Ockenfuss@users.noreply.github.com>
2023-11-03 01:42:16 +0100

    provie columnar transformation

--- src/elmesh/__init__.py
+++ src/elmesh/__init__.py
@@ -1,1 +1,1 @@
-from .Elmesh import elevation_body, regular_elevation_body, elevation_mesh
+from .Elmesh import elevation_body, regular_elevation_body, elevation_mesh, transform_to_columnar_elevation
